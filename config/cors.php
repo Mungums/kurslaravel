f0574f8f62ca027b3eb0f072d9db9e93998b2e76
Mungums <maxsboris18russ@gmail.com>
2025-06-11 13:10:54 +0400

    initial commit

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'register', 'logout'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['https://kursovaya-gamma.vercel.app'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
