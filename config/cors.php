c061043c22dcc6caaee010992ac437c1595542d7
Mungums <maxsboris18russ@gmail.com>
2025-06-29 12:43:32 +0400

    Update cors.php

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,7 @@
 return [
     'paths' => ['api/*', 'sanctum/csrf-cookie', 'login', 'register', 'logout'],
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['https://kursovaya-gamma.vercel.app'],
+    'allowed_origins' => ['*'],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
